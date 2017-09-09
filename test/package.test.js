817bd6cb5f974370d79290e51da09b4a7003b27c
Jared Hanson <jaredhanson@gmail.com>
2017-09-08 18:39:06 -0700

    Expose grant.

--- test/package.test.js
+++ test/package.test.js
@@ -6,8 +6,9 @@ var expect = require('chai').expect;
 
 describe('oauth2orize-acdc', function() {
   
-  it('should export hello world', function() {
-    expect(pkg.hello).to.equal('world');
+  it('should export grants', function() {
+    expect(pkg.grant).to.be.an('object');
+    expect(pkg.grant.acdc).to.be.a('function');
   });
   
 });
