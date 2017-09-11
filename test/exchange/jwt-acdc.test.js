3fdcf6797d2c856bd73a82813cc870189997fdba
Jared Hanson <jaredhanson@gmail.com>
2017-09-11 10:20:56 -0700

    More test cases.

--- test/exchange/jwt-acdc.test.js
+++ test/exchange/jwt-acdc.test.js
@@ -179,4 +179,74 @@ describe('exchange.jwt-acdc', function() {
     });
   });
   
+  describe('issuing an access token based on verifier', function() {
+    var response, err;
+
+    before(function(done) {
+      function issue(client, assertion, verifier, done) {
+        if (client.id !== '1') { return done(new Error('incorrect client argument')); }
+        if (assertion !== 'eyJ') { return done(new Error('incorrect code argument')); }
+        if (verifier !== 'dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk') { return done(new Error('incorrect code argument')); }
+        
+        return done(null, 's3cr1t');
+      }
+      
+      chai.connect.use(acdc(issue))
+        .req(function(req) {
+          req.user = { id: '1', name: 'OAuth Client' };
+          req.body = { assertion: 'eyJ', code_verifier: 'dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk' };
+        })
+        .end(function(res) {
+          response = res;
+          done();
+        })
+        .dispatch();
+    });
+    
+    it('should respond with headers', function() {
+      expect(response.getHeader('Content-Type')).to.equal('application/json');
+      expect(response.getHeader('Cache-Control')).to.equal('no-store');
+      expect(response.getHeader('Pragma')).to.equal('no-cache');
+    });
+    
+    it('should respond with body', function() {
+      expect(response.body).to.equal('{"access_token":"s3cr1t","token_type":"Bearer"}');
+    });
+  });
+  
+  describe('issuing an access token based on misspelled verifier', function() {
+    var response, err;
+
+    before(function(done) {
+      function issue(client, assertion, verifier, done) {
+        if (client.id !== '1') { return done(new Error('incorrect client argument')); }
+        if (assertion !== 'eyJ') { return done(new Error('incorrect code argument')); }
+        if (verifier !== 'dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk') { return done(new Error('incorrect code argument')); }
+        
+        return done(null, 's3cr1t');
+      }
+      
+      chai.connect.use(acdc(issue))
+        .req(function(req) {
+          req.user = { id: '1', name: 'OAuth Client' };
+          req.body = { assertion: 'eyJ', code_verifyer: 'dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk' };
+        })
+        .end(function(res) {
+          response = res;
+          done();
+        })
+        .dispatch();
+    });
+    
+    it('should respond with headers', function() {
+      expect(response.getHeader('Content-Type')).to.equal('application/json');
+      expect(response.getHeader('Cache-Control')).to.equal('no-store');
+      expect(response.getHeader('Pragma')).to.equal('no-cache');
+    });
+    
+    it('should respond with body', function() {
+      expect(response.body).to.equal('{"access_token":"s3cr1t","token_type":"Bearer"}');
+    });
+  });
+  
 });
